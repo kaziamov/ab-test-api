8e4c0bdf4f2284f43732b05ceaa119a48cc201b9
Ilia Kaziamov <kaziamov@outlook.com>
2023-05-06 10:26:47 +0300

    Add execute from db

--- ab_test_api/routing.py
+++ ab_test_api/routing.py
@@ -1,15 +1,16 @@
 from fastapi import Header
 from fastapi import APIRouter
 
+from ab_test_api.crud import get_device, add_device, test_select
+# from ab_test_api.db_connect import conn_pool
 
 router = APIRouter()
 
 
 @router.get("/api/v1/get-test/")
 async def tests(device_token=Header(None, alias='Device-Token')):
-    if device_token == '123':
-        return {"message": "Something else"}
-    return {"message": "Hello World"}
+    device = test_select(device_token)
+    return {"device_token": device_token, "message": device }
 
 
 @router.get("/")
