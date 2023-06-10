44f9c404e39beec55d20081b8a6fbfaba87fcd7d
Ilia Kaziamov <kaziamov@outlook.com>
2023-06-10 21:25:48 +0300

    Add min_conn and max_conn variables

--- ab_test_api/settings.py
+++ ab_test_api/settings.py
@@ -10,6 +10,9 @@ DB_NAME = os.getenv('DB_NAME')
 DB_USER = os.getenv('DB_USER')
 DB_PASS = os.getenv('DB_PASS')
 
+MIN_CONN = int(os.getenv('MIN_CONN', 1))
+MAX_CONN = int(os.getenv('MAX_CONN', 20))
+
 DATABASE_URL = urlparse(os.getenv('DATABASE_URL'))
 
 if DATABASE_URL:
